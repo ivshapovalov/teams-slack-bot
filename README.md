d739d4f13a728d5aeca8d4c9b50232c63c8ec5cf
starikivan <Ubnkth99>
2017-07-10 14:27:32 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Teams slack bot
+
+The slack bot preprocess commands from slack chat and constructs requests to the Teams microservice. And also construct successful/unsuccessful responses to the slack chat.
+
+[doc](https://github.com/JujaLabs/docs/tree/master/architecture/teams_slackbot)
---
+++ src/main/java/ua/com/juja/microservices/teams/slackbot/TeamSlackBotApplication.java
@@ -0,0 +1,12 @@
+package ua.com.juja.microservices.teams.slackbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamSlackBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamSlackBotApplication.class);
+    }
+}
