9163bf3fa4ba018bf244fc1df719f8e155b3159b
Ivan Shapovalov <ivshapovalov@gmail.com>
2017-07-29 14:24:04 +0300

    #5 - add UserService tests

---
+++ src/test/java/ua/com/juja/microservices/teams/slackbot/service/DefaultUserServiceTest.java
@@ -0,0 +1,66 @@
+package ua.com.juja.microservices.teams.slackbot.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import ua.com.juja.microservices.teams.slackbot.dao.UserRepository;
+import ua.com.juja.microservices.teams.slackbot.model.UserDTO;
+
+import javax.inject.Inject;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Matchers.anyObject;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+/**
+ * @author Ivan Shapovalov
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DefaultUserServiceTest {
+
+    @Inject
+    private UserService userService;
+
+    @MockBean
+    private UserRepository userRepository;
+
+    @Test
+    public void findUsersBySlackNamesExecutedCorrectly() throws Exception {
+        //given
+        List<String> slackNamesRequest = Arrays.asList(new String[]{"@user11", "@user22"});
+        List<UserDTO> expected = Arrays.asList(new UserDTO[]{new UserDTO("uuid1", "@user11"),
+                new UserDTO("uuid2", "user2")});
+        given(userRepository.findUsersBySlackNames(slackNamesRequest)).willReturn(expected);
+        //when
+        List<UserDTO> result = userService.findUsersBySlackNames(slackNamesRequest);
+        //then
+        assertThat(expected, is(result));
+        verify(userRepository).findUsersBySlackNames(slackNamesRequest);
+        verifyNoMoreInteractions(userRepository);
+    }
+
+    @Test
+    public void findUsersByUuidsExecutedCorrectly() throws Exception {
+        //given
+        List<String> uuidsRequest = Arrays.asList(new String[]{"uuid1", "uuid2"});
+        List<UserDTO> expected = Arrays.asList(new UserDTO[]{new UserDTO("uuid1", "@user11"),
+                new UserDTO("uuid2", "user2")});
+        given(userRepository.findUsersByUuids(uuidsRequest)).willReturn(expected);
+        //when
+        List<UserDTO> result = userService.findUsersByUuids(uuidsRequest);
+        //then
+        assertThat(expected, is(result));
+        verify(userRepository).findUsersByUuids(uuidsRequest);
+        verifyNoMoreInteractions(userRepository);
+    }
+}
