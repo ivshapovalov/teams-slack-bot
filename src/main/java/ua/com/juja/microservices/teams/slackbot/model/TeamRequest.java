bc19bfb1c48f3efa039b897827ae3fa2a00f5334
Ivan Shapovalov <ivshapovalov@gmail.com>
2017-08-02 16:16:47 +0300

    #5 - refactor TeamRequest

--- src/main/java/ua/com/juja/microservices/teams/slackbot/model/TeamRequest.java
+++ src/main/java/ua/com/juja/microservices/teams/slackbot/model/TeamRequest.java
@@ -3,7 +3,6 @@ package ua.com.juja.microservices.teams.slackbot.model;
 import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
 import org.hibernate.validator.constraints.NotEmpty;
-import ua.com.juja.microservices.teams.slackbot.exceptions.WrongCommandFormatException;
 
 import java.util.Collections;
 import java.util.Set;
@@ -14,19 +13,11 @@ import java.util.Set;
 @ToString
 @Slf4j
 public class TeamRequest {
-    private static final int TEAM_SIZE = 4;
     @NotEmpty
     private Set<String> members;
 
     public TeamRequest(Set<String> members) {
-        log.debug("Started creating TeamRequest");
-        if (members.size() != TEAM_SIZE) {
-            log.warn("Members size is not equals '{}'" + TEAM_SIZE);
-            throw new WrongCommandFormatException(String.format("We found %d slack names in your command." +
-                    " But size of the team must be %s.", members.size(), TEAM_SIZE));
-        }
         this.members = members;
-        log.debug("Finished creating new TeamRequest '{}'", this);
         log.info("Finished creating new TeamRequest");
     }
 
--- src/main/java/ua/com/juja/microservices/teams/slackbot/service/impl/TeamSlackbotServiceImpl.java
+++ src/main/java/ua/com/juja/microservices/teams/slackbot/service/impl/TeamSlackbotServiceImpl.java
@@ -2,6 +2,7 @@ package ua.com.juja.microservices.teams.slackbot.service.impl;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import ua.com.juja.microservices.teams.slackbot.exceptions.WrongCommandFormatException;
 import ua.com.juja.microservices.teams.slackbot.model.Team;
 import ua.com.juja.microservices.teams.slackbot.model.TeamRequest;
 import ua.com.juja.microservices.teams.slackbot.service.TeamService;
@@ -16,6 +17,8 @@ import java.util.Set;
 @Slf4j
 @Service
 public class TeamSlackbotServiceImpl implements TeamSlackbotService {
+
+    private static final int TEAM_SIZE = 4;
 
     private final SlackNameHandlerService slackNameHandlerService;
 
@@ -34,6 +37,11 @@ public class TeamSlackbotServiceImpl implements TeamSlackbotService {
         Set<String> members = slackNameHandlerService.getUuidsFromText(text);
         log.debug("Finished extract members '{}' from text '{}'", members, text);
 
+        if (members.size() != TEAM_SIZE) {
+            log.warn("Members size is not equals '{}'" + TEAM_SIZE);
+            throw new WrongCommandFormatException(String.format("We found %d slack names in your command." +
+                    " But size of the team must be %s.", members.size(), TEAM_SIZE));
+        }
         log.debug("Started create TeamRequest");
         TeamRequest teamRequest = new TeamRequest(members);
         log.debug("Finished create TeamRequest");
