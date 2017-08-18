12781f1e2974d5ac923414503f7469a4b769081d
Ivan Shapovalov <ivshapovalov@gmail.com>
2017-08-18 09:16:22 +0300

    #15 - add comments to aspect integration test

--- src/test/java/ua/com/juja/microservices/integration/AspectLoggerIntegrationTest.java
+++ src/test/java/ua/com/juja/microservices/integration/AspectLoggerIntegrationTest.java
@@ -9,5 +9,8 @@ public class AspectLoggerIntegrationTest {
     //TODO Replace with real test
     @Test
     public void test() throws Exception {
+        //    Are your pointcuts matching what you expect?
+        //    Do your advice reference the right pointcut?
+        //    Does the advice do as you expect?
     }
 }
