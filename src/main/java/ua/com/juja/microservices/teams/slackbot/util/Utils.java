4c974015b67dd19657085e866695902bf781e2be
Ivan Shapovalov <ivshapovalov@gmail.com>
2017-10-29 14:17:45 +0300

    #38 Utils refactor

--- src/main/java/ua/com/juja/microservices/teams/slackbot/util/Utils.java
+++ src/main/java/ua/com/juja/microservices/teams/slackbot/util/Utils.java
@@ -2,9 +2,6 @@ package ua.com.juja.microservices.teams.slackbot.util;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.web.client.HttpClientErrorException;
 import ua.com.juja.microservices.teams.slackbot.exceptions.ApiError;
 
 import java.io.IOException;
@@ -25,25 +22,28 @@ public class Utils {
         }
     }
 
-    public static HttpHeaders setupJsonHttpHeaders() {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
-        return headers;
-    }
-
-    public static ApiError convertToApiError(HttpClientErrorException httpClientErrorException) {
-        ObjectMapper mapper = new ObjectMapper();
-        try {
-            return mapper.readValue(httpClientErrorException.getResponseBodyAsString(), ApiError.class);
-        } catch (IOException e) {
-            return new ApiError(
-                    500, "BotInternalError",
-                    "I'm, sorry. I cannot parse api error message from remote service :(",
-                    "Cannot parse api error message from remote service",
-                    e.getMessage(),
-                    Collections.singletonList(httpClientErrorException.getMessage())
-            );
+    public static ApiError convertToApiError(String message) {
+        int contentExists = message.indexOf("content:");
+        ApiError apiError = new ApiError(
+                500, "BotInternalError",
+                "I'm, sorry. I cannot parse api error message from remote service :(",
+                "Cannot parse api error message from remote service",
+                message, Collections.singletonList(message));
+        if (contentExists != -1) {
+            String apiMessage = message.substring(contentExists + 8);
+            ObjectMapper mapper = new ObjectMapper();
+            try {
+                apiError = mapper.readValue(apiMessage, ApiError.class);
+            } catch (IOException e) {
+                apiError = new ApiError(
+                        500, "BotInternalError",
+                        "I'm, sorry. I cannot parse api error message from remote service :(",
+                        "Cannot parse api error message from remote service",
+                        e.getMessage(), Collections.singletonList(message)
+                );
+            }
         }
+        return apiError;
     }
 
     private static String listToStringWithDelimeter(Set<String> list, String delimeter) {
--- src/test/java/ua/com/juja/microservices/utils/TestUtils.java
+++ src/test/java/ua/com/juja/microservices/utils/TestUtils.java
@@ -2,21 +2,36 @@ package ua.com.juja.microservices.utils;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.io.IOException;
-import java.io.Reader;
-
 /**
  * @author Ivan Shapovalov
  */
 @Slf4j
 public class TestUtils {
-    public static String convertToString(Reader reader) throws IOException {
-        char[] arr = new char[8 * 1024];
-        StringBuilder buffer = new StringBuilder();
-        int numCharsRead;
-        while ((numCharsRead = reader.read(arr, 0, arr.length)) != -1) {
-            buffer.append(arr, 0, numCharsRead);
-        }
-        return buffer.toString();
+
+    public static String getUrlTemplate(String endpoint) {
+        return endpoint + "?" +
+                "token={slashCommandToken}&" +
+                "team_id={team_id}&" +
+                "team_domain={team_domain}&" +
+                "channel_id={channel_id}&" +
+                "channel_name={channel_name}&" +
+                "user_id={user_id}&" +
+                "user_name={user_name}&" +
+                "command={command}&" +
+                "text={text}&" +
+                "response_url={response_url}&";
+    }
+
+    public static Object[] getUriVars(String slackToken, String command, String description, String responseUrl) {
+        return new Object[]{slackToken,
+                "any_team_id",
+                "any_domain",
+                "UHASHB8JB",
+                "test-channel",
+                "UNJSD9OKM",
+                "@slack-from",
+                command,
+                description,
+                responseUrl};
     }
 }
